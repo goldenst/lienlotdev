3a497ff87647111577a653dca3500d517edec3a3
MSI\jeff <hatcher28@me.com>
2019-09-02 01:05:15 -0700

    dynamic html  listinfs and single listing

---
+++ listings/choices.py
@@ -0,0 +1,14 @@
+type_choices = {
+'cars' : 'Cars',
+'trucks': 'Trucks',
+'suv': 'Suv',
+'vans': 'Vans'
+}
+
+year_choices = {
+  '1970': 1970,
+}
+
+make_choices = {
+  'acura': 'acura'
+}
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import type_choices , year_choices, make_choices
+
+from listings.models import Listing
+from sellers.models import Seller
 
 # Create your views here.
 def index(request):
-  return render(request, 'pages/index.html')
+  listings = Listing.objects.order_by('-avalible_date').filter(is_published=True)[:3]
+
+  context = {
+    'listings': listings,
+    'type_choices': type_choices,
+    'year_choices': year_choices,
+    'make_choices' : make_choices
+  }
+  return render(request, 'pages/index.html', context)
 
 
 def about(request):
-  return render(request, 'pages/about.html')
+  # Get sellers
+  sellers = Seller.objects.all()
+
+  context = {
+    'sellers': sellers
+  }
+  return render(request, 'pages/about.html', context)
